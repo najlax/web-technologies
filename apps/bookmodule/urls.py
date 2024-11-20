68fbae00a370bc5c4f8a4b16265aeb4f3b6d5171
Pixel <pixel@webmail.com>
2024-11-20 17:19:51 +0300

    lab 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- path('', views.index),
+ path('', views.index, name= "books.index"),
  path('index2/<int:val1>/', views.index2),
  path('<int:bookId>', views.viewbook),
  path('html5/links/', views.links),
  path('html5/text/formatting/', views.formatting),
  path('html5/listing/', views.listing),
- path('html5/tables/', views.tables)
+ path('html5/tables/', views.tables),
+ path('list_books/', views.list_books, name="books.list_books"),
+ path('aboutus/', views.aboutus, name="books.aboutus"),
+ path('list_books/one_book/',views.viewbook),
+ path('search/', views.search_books)
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -29,3 +29,49 @@ def listing(request):
 
 def tables(request):
  return render(request, "table.html")
+
+def index(request):
+ return render(request,"bookmodule/index.html")
+
+def list_books(request):
+ return render(request,"bookmodule/list_books.html")
+
+def viewbook(request):
+ return render(request,"bookmodule/one_book.html")
+
+def aboutus(request):
+ return render(request,"bookmodule/aboutus.html")
+
+def search(request):
+ return render(request, "bookmodule/search.html")
+
+
+def __getBooksList(): 
+  book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'} 
+  book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'} 
+  book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'} 
+  return [book1, book2, book3] 
+
+  
+
+
+def search_books(request):
+    if request.method == "POST":
+        keyword = request.POST.get('keyword').lower()
+        search_title = request.POST.get('option1')
+        search_author = request.POST.get('option2')
+
+        # Filter books
+        books = __getBooksList()
+        filtered_books = []
+        for book in books:
+            found = False
+            if search_title and keyword in book['title'].lower():
+                found = True
+            if not found and search_author and keyword in book['author'].lower():
+                found = True
+            if found:
+                filtered_books.append(book)
+
+        return render(request, 'bookmodule/bookList.html', {'books': filtered_books})
+    return render(request, 'bookmodule/search.html')
